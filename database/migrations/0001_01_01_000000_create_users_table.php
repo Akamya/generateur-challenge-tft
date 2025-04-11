5e2a0b05935e8e58515465d8b558aff4251ff1a9
Akamya <elodie.langlet@gmail.com>
2025-04-11 18:16:01 +0200

    feat(migrations): add columns to users table

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -13,7 +13,10 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('username');
+            $table->boolean('is_admin')->default(false);
+            $table->integer('score')->default(0);
+            $table->string('riot_username')->nullable();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
