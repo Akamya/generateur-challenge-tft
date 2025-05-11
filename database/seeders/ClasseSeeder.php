1b8d23d256e1330991e0298bc954a79c3c066633
Akamya <elodie.langlet@gmail.com>
2025-05-11 20:36:00 +0200

    seed icons not in gitignore

--- database/seeders/ClasseSeeder.php
+++ database/seeders/ClasseSeeder.php
@@ -14,17 +14,17 @@ class ClasseSeeder extends Seeder
     public function run(): void
     {
         $classes = [
-            ['description' => 'Reach level bronze', 'name' => 'A.M.P.', 'technical_name' => 'TFT14_Supercharge'],
-            ['description' => 'Reach level bronze', 'name' => 'Bastion', 'technical_name' => 'TFT14_Armorclad'],
-            ['description' => 'Reach level bronze', 'name' => 'Bruiser', 'technical_name' => 'TFT14_Bruiser'],
-            ['description' => 'Reach level bronze', 'name' => 'Dynamo', 'technical_name' => 'TFT14_Thirsty'],
-            ['description' => 'Reach level bronze', 'name' => 'Executioner', 'technical_name' => 'TFT14_Cutter'],
-            ['description' => 'Reach level bronze', 'name' => 'Marksman', 'technical_name' => 'TFT14_Marksman'],
-            ['description' => 'Reach level bronze', 'name' => 'Rapidfire', 'technical_name' => 'TFT14_Swift'],
-            ['description' => 'Reach level bronze', 'name' => 'Slayer', 'technical_name' => 'TFT14_Strong'],
-            ['description' => 'Reach level bronze', 'name' => 'Strategist', 'technical_name' => 'TFT14_Controller'],
-            ['description' => 'Reach level bronze', 'name' => 'Techie', 'technical_name' => 'TFT14_Techie'],
-            ['description' => 'Reach level bronze', 'name' => 'Vanguard', 'technical_name' => 'TFT14_Vanguard'],
+            ['description' => 'Reach level bronze', 'name' => 'A.M.P.', 'technical_name' => 'TFT14_Supercharge', 'image' => 'seed/classes/14_Amp.png'],
+            ['description' => 'Reach level bronze', 'name' => 'Bastion', 'technical_name' => 'TFT14_Armorclad', 'image' => 'seed/classes/14_Bastion.png'],
+            ['description' => 'Reach level bronze', 'name' => 'Bruiser', 'technical_name' => 'TFT14_Bruiser', 'image' => 'seed/classes/14_Bruiser.png'],
+            ['description' => 'Reach level bronze', 'name' => 'Dynamo', 'technical_name' => 'TFT14_Thirsty', 'image' => 'seed/classes/14_Dynamo.png'],
+            ['description' => 'Reach level bronze', 'name' => 'Executioner', 'technical_name' => 'TFT14_Cutter', 'image' => 'seed/classes/14_Executioner.png'],
+            ['description' => 'Reach level bronze', 'name' => 'Marksman', 'technical_name' => 'TFT14_Marksman', 'image' => 'seed/classes/14_Marksman.png'],
+            ['description' => 'Reach level bronze', 'name' => 'Rapidfire', 'technical_name' => 'TFT14_Swift', 'image' => 'seed/classes/14_Rapidfire.png'],
+            ['description' => 'Reach level bronze', 'name' => 'Slayer', 'technical_name' => 'TFT14_Strong', 'image' => 'seed/classes/14_Slayer.png'],
+            ['description' => 'Reach level bronze', 'name' => 'Strategist', 'technical_name' => 'TFT14_Controller', 'image' => 'seed/classes/14_Strategist.png'],
+            ['description' => 'Reach level bronze', 'name' => 'Techie', 'technical_name' => 'TFT14_Techie', 'image' => 'seed/classes/14_Techie.png'],
+            ['description' => 'Reach level bronze', 'name' => 'Vanguard', 'technical_name' => 'TFT14_Vanguard', 'image' => 'seed/classes/14_Vanguard.png'],
         ];
 
         foreach ($classes as $classe) {
@@ -32,7 +32,8 @@ class ClasseSeeder extends Seeder
                 'name' => $classe["name"],
                 'technical_name' => $classe["technical_name"],
                 'description' => $classe["description"],
-                'season_id' => Season::first()->id
+                'season_id' => Season::first()->id,
+                'image' => $classe["image"],
             ]);
         }
     }
--- database/seeders/OriginSeeder.php
+++ database/seeders/OriginSeeder.php
@@ -14,20 +14,20 @@ class OriginSeeder extends Seeder
     public function run(): void
     {
         $origins = [
-            ['description' => 'Reach level gold', 'name' => 'Anima Squad', 'technical_name' => 'TFT14_AnimaSqua'],
-            ['description' => 'Reach level gold', 'name' => 'BoomBots', 'technical_name' => 'TFT14_BallisTek'],
-            ['description' => 'Reach level gold', 'name' => 'Cyberboss', 'technical_name' => 'TFT14_Cyberbos'],
-            ['description' => 'Reach level gold', 'name' => 'Cypher', 'technical_name' => 'TFT14_Suits'],
-            ['description' => 'Reach level gold', 'name' => 'Divincorp', 'technical_name' => 'TFT14_Divinicorp'],
-            ['description' => 'Reach level gold', 'name' => 'Exotech', 'technical_name' => 'TFT14_EdgeRunner'],
-            ['description' => 'Reach level gold', 'name' => 'God of the Net', 'technical_name' => 'TFT14_Netgod'],
-            ['description' => 'Reach level gold', 'name' => 'Golden Ox', 'technical_name' => 'TFT14_Immortal'],
-            ['description' => 'Reach level gold', 'name' => 'Nitro', 'technical_name' => 'TFT14_HotRod'],
-            ['description' => 'Reach level gold', 'name' => 'Overlord', 'technical_name' => 'TFT14_Overlord'],
-            ['description' => 'Reach level gold', 'name' => 'Soul Killer', 'technical_name' => 'TFT14_ViegoUnique'],
-            ['description' => 'Reach level gold', 'name' => 'Street Demon', 'technical_name' => 'TFT14_StreetDemon'],
-            ['description' => 'Reach level gold', 'name' => 'Syndicate', 'technical_name' => 'TFT14_Mob'],
-            ['description' => 'Reach level gold', 'name' => 'Virus', 'technical_name' => 'TFT14_Virus'],
+            ['description' => 'Reach level gold', 'name' => 'Anima Squad', 'technical_name' => 'TFT14_AnimaSqua', 'image' => 'seed/origins/14_AnimaSquad.png'],
+            ['description' => 'Reach level gold', 'name' => 'BoomBots', 'technical_name' => 'TFT14_BallisTek', 'image' => 'seed/origins/14_BoomBots.png'],
+            ['description' => 'Reach level gold', 'name' => 'Cyberboss', 'technical_name' => 'TFT14_Cyberbos', 'image' => 'seed/origins/14_Cyberbosses.png'],
+            ['description' => 'Reach level gold', 'name' => 'Cypher', 'technical_name' => 'TFT14_Suits', 'image' => 'seed/origins/14_Cypher.png'],
+            ['description' => 'Reach level gold', 'name' => 'Divincorp', 'technical_name' => 'TFT14_Divinicorp', 'image' => 'seed/origins/14_Divincorp.png'],
+            ['description' => 'Reach level gold', 'name' => 'Exotech', 'technical_name' => 'TFT14_EdgeRunner', 'image' => 'seed/origins/14_Exotech.png'],
+            ['description' => 'Reach level gold', 'name' => 'God of the Net', 'technical_name' => 'TFT14_Netgod', 'image' => 'seed/origins/14_GodoftheNet.png'],
+            ['description' => 'Reach level gold', 'name' => 'Golden Ox', 'technical_name' => 'TFT14_Immortal', 'image' => 'seed/origins/14_GoldenOx.png'],
+            ['description' => 'Reach level gold', 'name' => 'Nitro', 'technical_name' => 'TFT14_HotRod', 'image' => 'seed/origins/14_NitroForge.png'],
+            ['description' => 'Reach level gold', 'name' => 'Overlord', 'technical_name' => 'TFT14_Overlord', 'image' => 'seed/origins/14_Overlord.png'],
+            ['description' => 'Reach level gold', 'name' => 'Soul Killer', 'technical_name' => 'TFT14_ViegoUnique', 'image' => 'seed/origins/14_SoulKiller.png'],
+            ['description' => 'Reach level gold', 'name' => 'Street Demon', 'technical_name' => 'TFT14_StreetDemon', 'image' => 'seed/origins/14_StreetDemon.png'],
+            ['description' => 'Reach level gold', 'name' => 'Syndicate', 'technical_name' => 'TFT14_Mob', 'image' => 'seed/origins/14_Mob.png'],
+            ['description' => 'Reach level gold', 'name' => 'Virus', 'technical_name' => 'TFT14_Virus', 'image' => 'seed/origins/14_Virus.png'],
         ];
 
         foreach ($origins as $origin) {
@@ -35,7 +35,8 @@ class OriginSeeder extends Seeder
                 'name' => $origin["name"],
                 'technical_name' => $origin["technical_name"],
                 'description' => $origin["description"],
-                'season_id' => Season::first()->id
+                'season_id' => Season::first()->id,
+                'image' => $origin["image"],
             ]);
         }
     }
