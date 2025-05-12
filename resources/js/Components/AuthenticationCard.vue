b496a82b8561d6a5b110b3c4dc0cc7eb2d74576f
Akamya <elodie.langlet@gmail.com>
2025-05-12 18:27:43 +0200

    register bg

--- resources/js/Components/AuthenticationCard.vue
+++ resources/js/Components/AuthenticationCard.vue
@@ -4,16 +4,18 @@
     >
         <div class="flex flex-col items-center">
             <slot name="logo" />
+
             <h1
                 class="text-primary-light text-5xl font-bold mt-5 mb-2 font-parkinsans text-center"
             >
                 TFT OPERATION
             </h1>
+
             <div class="w-96 h-1 bg-primary-first mb-6"></div>
         </div>
 
         <div
-            class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white/80 backdrop-blur-md shadow-md overflow-hidden sm:rounded-lg"
+            class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white/80 backdrop-blur-md shadow-md overflow-hidden sm:rounded-lg transition ease-in-out duration-300"
         >
             <slot />
         </div>
